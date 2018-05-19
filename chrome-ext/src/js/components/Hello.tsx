7c1b32e04f9a19fdeb446b4b0ac7a95144bb7751
baurine <2008.hbl@gmail.com>
2018-05-19 17:47:10 +0800

    enable allowSyntheticDefaultImports to support use `import React from 'react'`
    - it seems don't work

--- chrome-ext/src/js/components/Hello.tsx
+++ chrome-ext/src/js/components/Hello.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react'
+// import * as React from 'react'
+import React from 'react'
 
 export interface HelloProps { compiler: string; framework: string }
 
--- chrome-ext/src/js/ts-index.tsx
+++ chrome-ext/src/js/ts-index.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+// import * as React from 'react'
+// import * as ReactDOM from 'react-dom'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import { Hello } from './components/Hello'
 
