78c783921afd67bdf910d70f6f63f08ccc43395e
baurine <2008.hbl@gmail.com>
2018-05-26 16:52:52 +0800

    work as chrome extension

--- chrome-ext/src/js/time_logger.tsx
+++ chrome-ext/src/js/time_logger.tsx
@@ -3,7 +3,34 @@ import * as ReactDOM from 'react-dom'
 
 import TimeLoggerBox from './components/TimeLoggerBox'
 
-ReactDOM.render(
-  <TimeLoggerBox/>,
-  document.getElementById('root')
-)
+// utils
+function log(msg: string) {
+  console.log("TimeLogger extension:", msg)
+}
+
+function main() {
+  log('load')
+  let pathname = document.location.pathname
+  if (!/\/issues\/\d+/.test(pathname)) {
+    log("this is not a issue page")
+    return
+  }
+  const notesContainer = document.getElementById('notes')
+  if (!notesContainer) {
+    log("there is no notes container")
+    return
+  }
+  const timeLoggerContainer = document.createElement('div')
+  timeLoggerContainer.id = 'time-logger-box'
+  notesContainer.insertBefore(timeLoggerContainer, notesContainer.lastChild)
+  renderTimeLogger(timeLoggerContainer)
+}
+
+function renderTimeLogger(container: HTMLElement) {
+  ReactDOM.render(
+    <TimeLoggerBox/>,
+    container
+  )
+}
+
+main()
