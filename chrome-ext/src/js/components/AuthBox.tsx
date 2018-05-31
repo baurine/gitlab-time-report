719cddd66d0f10e46fec8e54e19f215fb5b6a682
baurine <2008.hbl@gmail.com>
2018-05-31 22:24:37 +0800

    add MessageBox component

--- chrome-ext/src/js/components/AuthBox.tsx
+++ chrome-ext/src/js/components/AuthBox.tsx
@@ -4,6 +4,7 @@ const md5 = require('blueimp-md5')
 import { firebaseAuth, firebaseDb } from '../firebase/firebase'
 import { IAuthBoxProps, IAuthBoxState } from '../types'
 import CommonUtil from '../utils/common-util'
+import MessageBox from './MessageBox'
 require('../../css/AuthBox.scss')
 
 export default class AuthBox extends React.Component<IAuthBoxProps, IAuthBoxState> {
@@ -183,7 +184,7 @@ export default class AuthBox extends React.Component<IAuthBoxProps, IAuthBoxStat
     const { user, loading, message } = this.state
     return (
       <div className='auth-box-container'>
-        <p className='msg'>{message}</p>
+        <MessageBox message={message}/>
         { this.renderAuthInputs() }
       </div>
     )
---
+++ chrome-ext/src/js/components/MessageBox.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+require('../../css/MessageBox.scss')
+
+const MessageBox = (props: {message?: string, onClose?: ()=>void}) => {
+  const { message, onClose } = props
+  if (message) {
+    return (
+      <div className='message-box-container'>
+        <span className='message'>{message}</span>
+        {
+          onClose &&
+          <a onClick={onClose}>X</a>
+        }
+      </div>
+    )
+  }
+  return null
+}
+
+export default MessageBox
